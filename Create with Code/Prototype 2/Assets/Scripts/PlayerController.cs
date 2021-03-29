bf312a18261e58da8bbb74dd111c175e2a27d6d9
Cairo Batt <cairo.batt@gmail.com>
2021-03-29 13:47:04 -0600

    Lesson 2.2 complete

--- Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 19.0f;
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +29,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // Launch projectile from the player with space bar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
