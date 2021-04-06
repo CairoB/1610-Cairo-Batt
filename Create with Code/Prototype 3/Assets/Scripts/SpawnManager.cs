da0f1ef564de34a88fa18fb64d62376b59c5aa7d
Cairo Batt <cairo.batt@gmail.com>
2021-04-06 14:35:54 -0600

    Lesson 3.1 complete

---
+++ Create with Code/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private bool spawnReady = true;
+    private float repeatMin = 0.5f;
+    private float repeatMax = 2.5f;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnReady) {
+            spawnReady = false;
+            Invoke("SpawnObstacle", Random.Range(repeatMin, repeatMax));
+        }
+    }
+
+    void SpawnObstacle() {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        spawnReady = true;
+    }
+}
