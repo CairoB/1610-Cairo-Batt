b33b1deb67dd9d14d8aa9f180a8ad5a7225d2c78
Cairo Batt <cairo.batt@gmail.com>
2021-04-07 14:17:12 -0600

    Lesson 3.2 complete

--- Create with Code/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Create with Code/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -9,17 +9,18 @@ public class SpawnManager : MonoBehaviour
     private float repeatMin = 0.5f;
     private float repeatMax = 2.5f;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (spawnReady) {
+        if (spawnReady && playerControllerScript.gameOver == false) {
             spawnReady = false;
             Invoke("SpawnObstacle", Random.Range(repeatMin, repeatMax));
         }
