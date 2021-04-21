3a95f310ce49103cd88e85de82ce55a88ba81f3f
Cairo Batt <cairo.batt@gmail.com>
2021-04-21 13:33:28 -0600

    Lesson 4.2 complete

---
+++ Create with Code/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnX = Random.Range(-spawnRange, spawnRange);
+        float spawnZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomSpawn = new Vector3(spawnX, 15, spawnZ);
+        return randomSpawn;
+    }
+}
